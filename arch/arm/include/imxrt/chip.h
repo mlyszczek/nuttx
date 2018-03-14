93eb81d408fffb07f5f0f49ea194a49284bc584b
Gregory Nutt <gnutt@nuttx.org>
2018-03-14 15:07:30 -0600

    arch/arm/src/imxrt:  The i.MX Rt implements 4 bits of interrupt priority, not two.  Thanks, Janne.

--- arch/arm/include/imxrt/chip.h
+++ arch/arm/include/imxrt/chip.h
@@ -64,13 +64,12 @@
 #endif
 
 /* NVIC priority levels *************************************************************/
-/* Each priority field holds a priority value, 0-15. The lower the value, the greater
- * the priority of the corresponding interrupt. The processor implements only
- * bits[7:6] of each field, bits[5:0] read as zero and ignore writes.
+/* Each priority field holds an 8-bit priority value, 0-15. The lower the value, the
+ * greater the priority of the corresponding interrupt.  The i.MX RT processor
+ * implements only  bits[7:4] of each field, bits[3:0] read as zero and ignore writes.
  */
-#warning REVISIT:  Are these correct?
 
-#define NVIC_SYSH_PRIORITY_MIN        0xc0 /* All bits[7:6] set is minimum priority */
+#define NVIC_SYSH_PRIORITY_MIN        0xf0 /* All bits[7:4] set is minimum priority */
 #define NVIC_SYSH_PRIORITY_DEFAULT    0x80 /* Midpoint is the default */
 #define NVIC_SYSH_PRIORITY_MAX        0x00 /* Zero is maximum priority */
 #define NVIC_SYSH_PRIORITY_STEP       0x40 /* Two bits of interrupt priority used */
