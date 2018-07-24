4c5fae06dec969d1b262729bd9b5817a3b1ff550
Gregory Nutt <gnutt@nuttx.org>
2018-07-24 17:11:30 -0600

    Oops.. completed version of the PM header file was still in the editor.

--- arch/arm/src/samd5e5/chip/sam_pm.h
+++ arch/arm/src/samd5e5/chip/sam_pm.h
@@ -50,85 +50,87 @@
 
 /* PM register offsets ******************************************************************************/
 
-#define SAM_PM_CTRLA_OFFSET                0x0000  /* Control A */
-#define SAM_PM_SLEEPCFG_OFFSET             0x0001  /* Sleep configuration */
-#define SAM_PM_INTENCLR_OFFSET             0x0004  /* Interrupt enable clear register */
-#define SAM_PM_INTENSET_OFFSET             0x0005  /* Interrupt enable set register */
-#define SAM_PM_INTFLAG_OFFSET              0x0006  /* Interrupt flag status and clear register */
-#define SAM_PM_STDBYCFG_OFFSET             0x0008  /* Standy configuration */
-#define SAM_PM_HIBCFG_OFFSET               0x0009  /* Hibernate configuration */
-#define SAM_PM_BKUPCFG_OFFSET              0x000a  /* Backup configuration */
+#define SAM_PM_CTRLA_OFFSET              0x0000  /* Control A */
+#define SAM_PM_SLEEPCFG_OFFSET           0x0001  /* Sleep configuration */
+#define SAM_PM_INTENCLR_OFFSET           0x0004  /* Interrupt enable clear register */
+#define SAM_PM_INTENSET_OFFSET           0x0005  /* Interrupt enable set register */
+#define SAM_PM_INTFLAG_OFFSET            0x0006  /* Interrupt flag status and clear register */
+#define SAM_PM_STDBYCFG_OFFSET           0x0008  /* Standy configuration */
+#define SAM_PM_HIBCFG_OFFSET             0x0009  /* Hibernate configuration */
+#define SAM_PM_BKUPCFG_OFFSET            0x000a  /* Backup configuration */
 
 /* PM register addresses ****************************************************************************/
 
-#define SAM_PM_CTRLA                       (SAM_PM_BASE+SAM_PM_CTRLA_OFFSET)
-#define SAM_PM_SLEEPCFG                    (SAM_PM_BASE+SAM_PM_SLEEPCFG_OFFSET)
-#define SAM_PM_INTENCLR                    (SAM_PM_BASE+SAM_PM_INTENCLR_OFFSET)
-#define SAM_PM_INTENSET                    (SAM_PM_BASE+SAM_PM_INTENSET_OFFSET)
-#define SAM_PM_INTFLAG                     (SAM_PM_BASE+SAM_PM_INTFLAG_OFFSET)
-#define SAM_PM_STDBYCFG                    (SAM_PM_BASE+SAM_PM_STDBYCFG_OFFSET)
-#define SAM_PM_HIBCFG_OFFSET               0x0009  /* Hibernate configuration */
-#define SAM_PM_HIBCFG_OFFSET               0x0009  /* Hibernate configuration */
-#define SAM_PM_BKUPCFG_OFFSET              0x000a  /* Backup configuration */
-#define SAM_PM_BKUPCFG_OFFSET              0x000a  /* Backup configuration */
+#define SAM_PM_CTRLA                     (SAM_PM_BASE + SAM_PM_CTRLA_OFFSET)
+#define SAM_PM_SLEEPCFG                  (SAM_PM_BASE + SAM_PM_SLEEPCFG_OFFSET)
+#define SAM_PM_INTENCLR                  (SAM_PM_BASE + SAM_PM_INTENCLR_OFFSET)
+#define SAM_PM_INTENSET                  (SAM_PM_BASE + SAM_PM_INTENSET_OFFSET)
+#define SAM_PM_INTFLAG                   (SAM_PM_BASE + SAM_PM_INTFLAG_OFFSET)
+#define SAM_PM_STDBYCFG                  (SAM_PM_BASE + SAM_PM_STDBYCFG_OFFSET)
+#define SAM_PM_HIBCFG                    (SAM_PM_BASE + SAM_PM_HIBCFG_OFFSET)
+#define SAM_PM_BKUPCFG                   (SAM_PM_BASE + SAM_PM_BKUPCFG_OFFSET)
 
 /* PM register bit definitions **********************************************************************/
 
 /* Control A register */
 
-#define PM_CTRLA_IORET                     (1 << 2)  /* Bit 2: I/O retention */
+#define PM_CTRLA_IORET                   (1 << 2)  /* Bit 2: I/O retention */
 
 /* Sleep configuration register */
 
-#define PM_SLEEPCFG_MODE_SHIFT             (0)        /* Bits 0-2: Idle Mode Configuration */
-#define PM_SLEEPCFG_MODE_MASK              (7 << PM_SLEEPCFG_MODE_SHIFT)
-#  define PM_SLEEPCFG_MODE_IDLE            (2 << PM_SLEEPCFG_MODE_SHIFT) /* CPU, AHBx, APBx clocks OFF */
-#  define PM_SLEEPCFG_MODE_STANDBY         (4 << PM_SLEEPCFG_MODE_SHIFT) /* All clocks OFF, except sleepwalking peripherals */
-#  define PM_SLEEPCFG_MODE_BACKUP          (5 << PM_SLEEPCFG_MODE_SHIFT) /* Only backup domain is powered ON */
-#  define PM_SLEEPCFG_MODE_OFF             (6 << PM_SLEEPCFG_MODE_SHIFT) /* All power domains are powered OFF */
+#define PM_SLEEPCFG_MODE_SHIFT           (0)        /* Bits 0-2: Idle Mode Configuration */
+#define PM_SLEEPCFG_MODE_MASK            (7 << PM_SLEEPCFG_MODE_SHIFT)
+#  define PM_SLEEPCFG_MODE_IDLE          (2 << PM_SLEEPCFG_MODE_SHIFT) /* CPU, AHBx, APBx clocks OFF */
+#  define PM_SLEEPCFG_MODE_STANDBY       (4 << PM_SLEEPCFG_MODE_SHIFT) /* All clocks OFF */
+#  define PM_SLEEPCFG_MODE_HIBERNATE     (5 << PM_SLEEPCFG_MODE_SHIFT) /* Backup domain is ON as well as
+                                                                        * some PDRAMs */
+#  define PM_SLEEPCFG_MODE_BACKUP        (6 << PM_SLEEPCFG_MODE_SHIFT) /* Only backup domain is
+                                                                        * powered ON */
+#  define PM_SLEEPCFG_MODE_OFF           (7 << PM_SLEEPCFG_MODE_SHIFT) /* All power domains are
+                                                                        * powered OFF */
 
 /* Interrupt enable clear, Interrupt enable set, and Interrupt flag status and clear registers */
 
-#define PM_INT_PLRDY                       (1 << 0)  /* Bit 0: Performanc level ready */
+#define PM_INT_SLEEPRDY                  (1 << 0)  /* Bit 0: Sleep mode entry ready interrupt */
 
 /* Standy configuration */
 
-#define PM_STDBYCFG_PDCFG_SHIFT            (0)       /* Bits 0-1: Power domain configuration */
-#define PM_STDBYCFG_PDCFG_MASK             (3 << PM_STDBYCFG_PDCFG_SHIFT)
-#  define PM_STDBYCFG_PDCFG_DEFAULT        (0 << PM_STDBYCFG_PDCFG_SHIFT) /* All power domains handled by HW */
-#  define PM_STDBYCFG_PDCFG_PD01           (1 << PM_STDBYCFG_PDCFG_SHIFT) /* PD0 ACTIVE; PD1/2 handled by HW */
-#  define PM_STDBYCFG_PDCFG_PD12           (2 << PM_STDBYCFG_PDCFG_SHIFT) /* PD0/1 ACTIVE; PD2 handled by HW */
-#  define PM_STDBYCFG_PDCFG_PD012          (3 << PM_STDBYCFG_PDCFG_SHIFT) /* All power domains ACTIVE */
-#define PM_STDBYCFG_DPGPD0                 (1 << 4)  /* Bit 4:  Dynamic power gating for power domain 0 */
-#define PM_STDBYCFG_DPGPD1                 (1 << 5)  /* Bit 5:  Dynamic power gating for power domain 1 */
-#define PM_STDBYCFG_VREGSMOD_SHIFT         (6)       /* Bits 6-7: Linked power domain */
-#define PM_STDBYCFG_VREGSMOD_MASK          (3 << PM_STDBYCFG_VREGSMOD_SHIFT)
-#  define PM_STDBYCFG_VREGSMOD_AUTO        (0 << PM_STDBYCFG_VREGSMOD_SHIFT) /* Automatic mode */
-#  define PM_STDBYCFG_VREGSMOD_PERFORMANCE (1 << PM_STDBYCFG_VREGSMOD_SHIFT) /* Performance oriented */
-#  define PM_STDBYCFG_VREGSMOD_LP          (2 << PM_STDBYCFG_VREGSMOD_SHIFT) /* Low power consumption oriented */
-#define PM_STDBYCFG_LINKPD_SHIFT           (8)       /* Bits 8-9: */
-#define PM_STDBYCFG_LINKPD_MASK            (3 << PM_STDBYCFG_LINKPD_SHIFT)
-#  define PM_STDBYCFG_LINKPD_DEFAULT       (0 << PM_STDBYCFG_LINKPD_SHIFT) /* Power domains not linked */
-#  define PM_STDBYCFG_LINKPD_PD01          (1 << PM_STDBYCFG_LINKPD_SHIFT) /* Power domains P0/1 linked */
-#  define PM_STDBYCFG_LINKPD_PD12          (2 << PM_STDBYCFG_LINKPD_SHIFT) /* Power domains P1/P2 linked */
-#  define PM_STDBYCFG_LINKPD_PD012         (3 << PM_STDBYCFG_LINKPD_SHIFT) /* All power domains linked */
-#define PM_STDBYCFG_BBIASHS_SHIFT          (10)      /* Bits 10-11: Back bias for HMCRAMCHS */
-#define PM_STDBYCFG_BBIASHS_MASK           (3 << PM_STDBYCFG_BBIASHS_SHIFT)
-#  define PM_STDBYCFG_BBIASHS_RETBACK      (0 << PM_STDBYCFG_BBIASHS_SHIFT) /* Retention back biasing mode */
-#  define PM_STDBYCFG_BBIASHS_STDBYBACK    (1 << PM_STDBYCFG_BBIASHS_SHIFT) /* Standby back biasing mode */
-#  define PM_STDBYCFG_BBIASHS_STDBYOFF     (2 << PM_STDBYCFG_BBIASHS_SHIFT) /* Standby OFF mode */
-#  define PM_STDBYCFG_BBIASHS_OFF          (3 << PM_STDBYCFG_BBIASHS_SHIFT) /* Always OFF mode */
-#define PM_STDBYCFG_BBIASLP_SHIFT          (12)      /* Bits 12-13: Back bias for HMCRAMCLP */
-#define PM_STDBYCFG_BBIASLP_MASK           (3 << PM_STDBYCFG_BBIASLP_SHIFT)
-#  define PM_STDBYCFG_BBIASLP_RETBACK      (0 << PM_STDBYCFG_BBIASLP_SHIFT) /* Retention back biasing mode */
-#  define PM_STDBYCFG_BBIASLP_STDBYBACK    (1 << PM_STDBYCFG_BBIASLP_SHIFT) /* Standby back biasing mode */
-#  define PM_STDBYCFG_BBIASLP_STDBYOFF     (2 << PM_STDBYCFG_BBIASLP_SHIFT) /* Standby OFF mode */
-#  define PM_STDBYCFG_BBIASLP_OFF          (3 << PM_STDBYCFG_BBIASLP_SHIFT) /* Always OFF mode */
+#define PM_STDBYCFG_RAMCFG_SHIFT         (0)       /* Bits 0-1: RAM Configuration */
+#define PM_STDBYCFG_RAMCFG_MASK          (3 << PM_STDBYCFG_RAMCFG_SHIFT)
+#  define PM_STDBYCFG_RAMCFG_RET         (0 << PM_STDBYCFG_RAMCFG_SHIFT) /* System RAM is retained */
+#  define PM_STDBYCFG_RAMCFG_PARTIAL     (1 << PM_STDBYCFG_RAMCFG_SHIFT) /* PD0 ACTIVE; PD1/2 handled by HW */
+#  define PM_STDBYCFG_RAMCFG_OFF         (2 << PM_STDBYCFG_RAMCFG_SHIFT) /* Only the first 32Kb of system
+                                                                          * RAM is retained */
+#define PM_STDBYCFG_FASTWKUP_SHIFT       (4)       /* Bits 4-5: Fast Wakeup */
+#define PM_STDBYCFG_FASTWKUP_MASK        (3 << PM_STDBYCFG_FASTWKUP_SHIFT)
+#  define PM_STDBYCFG_FASTWKUP_NO        (0 << PM_STDBYCFG_FASTWKUP_SHIFT) /* Disabled */
+#  define PM_STDBYCFG_FASTWKUP_NVM       (1 << PM_STDBYCFG_FASTWKUP_SHIFT) /* Enabled on NVM */
+#  define PM_STDBYCFG_FASTWKUP_MAINVREG  (2 << PM_STDBYCFG_FASTWKUP_SHIFT) /* Enabled on MAINVREG */
+#  define PM_STDBYCFG_FASTWKUP_BOTH      (3 << PM_STDBYCFG_FASTWKUP_SHIFT) /* Enabled on both */
 
 /* Hibernate configuration */
-#define PM_HIBCFG_
+
+#define PM_HIBCFG_RAMCFG_SHIFT           (0)       /* Bits 0-1: RAM Configuration */
+#define PM_HIBCFG_RAMCFG_MASK            (3 << PM_HIBCFG_RAMCFG_SHIFT)
+#  define PM_HIBCFG_RAMCFG_RET           (0 << PM_HIBCFG_RAMCFG_SHIFT) /* System RAM is retained */
+#  define PM_HIBCFG_RAMCFG_PARTIAL       (1 << PM_HIBCFG_RAMCFG_SHIFT) /* PD0 ACTIVE; PD1/2 handled by HW */
+#  define PM_HIBCFG_RAMCFG_OFF           (2 << PM_HIBCFG_RAMCFG_SHIFT) /* Only the first 32Kb of system
+                                                                        * RAM is retained */
+#define PM_HIBCFG_BRAMCFG_SHIFT          (0)       /* Bits 0-1: Backup RAM Configuration */
+#define PM_HIBCFG_BRAMCFG_MASK           (3 << PM_HIBCFG_BRAMCFG_SHIFT)
+#  define PM_HIBCFG_BRAMCFG_RET          (0 << PM_HIBCFG_BRAMCFG_SHIFT) /* System RAM is retained */
+#  define PM_HIBCFG_BRAMCFG_PARTIAL      (1 << PM_HIBCFG_BRAMCFG_SHIFT) /* PD0 ACTIVE; PD1/2 handled by HW */
+#  define PM_HIBCFG_BRAMCFG_OFF          (2 << PM_HIBCFG_BRAMCFG_SHIFT) /* Only the first 32Kb of system
+                                                                           * RAM is retained */
+
 /* Backup configuration */
-#define PM_BKUPCFG_
+
+#define PM_BKUPCFG_BRAMCFG_SHIFT         (0)       /* Bits 0-1: RAM Configuration */
+#define PM_BKUPCFG_BRAMCFG_MASK          (3 << PM_BKUPCFG_BRAMCFG_SHIFT)
+#  define PM_BKUPCFG_BRAMCFG_RET         (0 << PM_BKUPCFG_BRAMCFG_SHIFT) /* System RAM is retained */
+#  define PM_BKUPCFG_BRAMCFG_PARTIAL     (1 << PM_BKUPCFG_BRAMCFG_SHIFT) /* PD0 ACTIVE; PD1/2 handled by HW */
+#  define PM_BKUPCFG_BRAMCFG_OFF         (2 << PM_BKUPCFG_BRAMCFG_SHIFT) /* Only the first 32Kb of system
+                                                                          * RAM is retained */
 
 /****************************************************************************************************
  * Public Types
