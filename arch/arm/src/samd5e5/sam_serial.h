2b18ad87921d6f33fd75ac08905a1e7f2e3c277e
Gregory Nutt <gnutt@nuttx.org>
2018-07-25 13:35:03 -0600

    arch/arm/src/samd5e5:  Bring all SERCOM USART logic from SAMD2L2 to SAMD5E5.  This is a brute coy with nothing more than more that name changes and extension from 5 to 7 SERCOMs.

--- arch/arm/src/samd5e5/sam_serial.h
+++ arch/arm/src/samd5e5/sam_serial.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/arm/src/samd5e5/sam_lowputc.h
+ * arch/arm/src/samd5e5/sam_serial.h
  *
  *   Copyright (C) 2018 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
@@ -33,14 +33,15 @@
  *
  ****************************************************************************/
 
-#ifndef __ARCH_ARM_SRC_SAMD5E5_SAM_LOWPUTC_H
-#define __ARCH_ARM_SRC_SAMD5E5_SAM_LOWPUTC_H
+#ifndef __ARCH_ARM_SRC_SAMD5E5_SAM_SERIAL_H
+#define __ARCH_ARM_SRC_SAMD5E5_SAM_SERIAL_H
 
 /****************************************************************************
  * Included Files
  ****************************************************************************/
 
 #include <nuttx/config.h>
+
 #include "sam_config.h"
 
 /****************************************************************************
@@ -55,76 +56,12 @@
  * Public Data
  ****************************************************************************/
 
-#ifndef __ASSEMBLY__
-
-#undef EXTERN
-#if defined(__cplusplus)
-#define EXTERN extern "C"
-extern "C"
-{
-#else
-#define EXTERN extern
-#endif
-
 /****************************************************************************
- * Public Function Prototypes
+ * Inline Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Name: sam_lowsetup
- *
- * Description:
- *   Called at the very beginning of _start.  Performs low level
- *   initialization.
- *
+ * Public Functions
  ****************************************************************************/
 
-void sam_lowsetup(void);
-
-/****************************************************************************
- * Name: sam_usart_initialize
- *
- * Description:
- *   Set the configuration of a SERCOM for provided USART configuration.
- *   This configures the SERCOM as a USART, but does not configure USART
- *   interrupts or enable the USART.
- *
- ****************************************************************************/
-
-#ifdef SAMD5E5_HAVE_USART
-struct sam_usart_config_s;
-int sam_usart_initialize(const struct sam_usart_config_s * const config);
-#endif
-
-/****************************************************************************
- * Name: sam_usart_reset
- *
- * Description:
- *   Reset the USART SERCOM.  This restores all SERCOM register to the
- *   initial state and disables the SERCOM.
- *
- ****************************************************************************/
-
-#ifdef SAMD5E5_HAVE_USART
-struct sam_usart_config_s;
-void sam_usart_reset(const struct sam_usart_config_s * const config);
-#endif
-
-/****************************************************************************
- * Name: sam_lowputc
- *
- * Description:
- *   Output one character to the USART using a simple polling method.
- *
- ****************************************************************************/
-
-#ifdef HAVE_SERIAL_CONSOLE
-void sam_lowputc(uint32_t ch);
-#endif
-
-#undef EXTERN
-#if defined(__cplusplus)
-}
-#endif
-#endif /* __ASSEMBLY__ */
-#endif /* __ARCH_ARM_SRC_SAMD5E5_SAM_LOWPUTC_H */
+#endif /* __ARCH_ARM_SRC_SAMD5E5_SAM_SERIAL_H */
