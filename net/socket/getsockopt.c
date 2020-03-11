4bfb16f7bc2722cf39ca961d272ddc2a52ae38f2
Peter van der Perk <peter.vanderperk@nxp.com>
2020-03-11 16:13:18 +0100

    Added support for SO_TIMESTAMP in socketlayer and SocketCAN     Cleanup FlexCAN driver driver     Disabled workqueue based TX in FlexCAN

--- net/socket/getsockopt.c
+++ net/socket/getsockopt.c
@@ -278,6 +278,19 @@ static int psock_socketlevel_option(FAR struct socket *psock, int option,
         }
         break;
 
+#ifdef CONFIG_NET_TIMESTAMP
+      case SO_TIMESTAMP:
+        {
+          if (*value_len != sizeof(int))
+            {
+              return -EINVAL;
+            }
+
+          *(FAR int *)value = (int)psock->s_timestamp;
+        }
+        break;
+#endif
+
       /* The following are not yet implemented
        * (return values other than {0,1})
        */
